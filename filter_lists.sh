8c66a4007a5f05524462aa4379c48c076b1fa79e
Christian Kubat <78682318+SirKubiac@users.noreply.github.com>
2025-04-26 08:32:01 +0200

    Add files via upload
    Script that does the merging, cleaning and uploading to Github.

---
+++ filter_lists.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export GIT_ASKPASS=echo
+export GIT_USERNAME=
+export GIT_PASSWORD=
+
+# Herunterladen der Listen
+wget https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/ultimate.txt -P /var/filter_lists/ -O /var/filter_lists/File_1.txt
+wget https://raw.githubusercontent.com/hagezi/dns-blocklists/main/share/microsoft.txt -P /var/filter_lists/ -O /var/filter_lists/File_2.txt
+wget https://raw.githubusercontent.com/SirKubiac/Block-Lists/refs/heads/main/ATR_White_List.txt -P /var/filter_lists/ -O /var/filter_lists/File_3.txt
+wget https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/doh-onlydomains.txt -P /var/filter_lists/ -O /var/filter_lists/DoH.txt
+
+# Entfernen von Kommentaren und Leerzeilen aus den Listen
+sed -i '/^#/d' /var/filter_lists/File_1.txt
+sed -i '/^\s*$/d' /var/filter_lists/File_1.txt
+
+sed -i '/^#/d' /var/filter_lists/File_2.txt
+sed -i '/^\s*$/d' /var/filter_lists/File_2.txt
+
+sed -i '/^#/d' /var/filter_lists/DoH.txt
+sed -i '/^\s*$/d' /var/filter_lists/DoH.txt
+
+# Entfernen der Einträge aus File_2.txt und File_3.txt
+grep -v -F -f /var/filter_lists/File_2.txt /var/filter_lists/File_1.txt | grep -v -F -f /var/filter_lists/File_3.txt > /var/filter_lists/Merged.txt
+
+# Verschieben der bereinigten Dateien
+mv /var/filter_lists/DoH.txt /var/www/html
+mv /var/filter_lists/Merged.txt /var/git/Block-Lists/Merged.txt
+rm /var/filter_lists/File_1.txt /var/filter_lists/File_2.txt /var/filter_lists/File_3.txt
+ 
+# Aktualisieren des Git-Repositories
+cd /var/git/Block-Lists
+git pull
+git add .
+git commit -m "automatic update"
+git push https://$GIT_USERNAME:$GIT_PASSWORD@github.com/SirKubiac/Block-Lists.git --all
+
+# Vorlagen
+#sed -i '/^\s*$/d' /var/filter_lists/	#Entfernt Leerzeilen
+#sed -i '/^#/d' /var/filter_lists/	#Entfernt Zeilen beginnend mit '#'
+#sed -i '/\.$/d' /var/filter_lists/	#Entfernt Zeilen endend mit '.'
+#sed -i '/\*/d' /var/filter_lists/	#Entfernt Zeilen, die '*' enthalten
