6cedb75301e922e6ebe082069a1d3c679ed65ed3
SirKubiac <christian_kubat@outlook.de>
2025-05-19 13:30:49 +0000

    automatic update

--- filterlist_adguard.sh
+++ filterlist_adguard.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-export GIT_ASKPASS=
-export GIT_USERNAME=
-export GIT_PASSWORD=
+export GIT_ASKPASS=echo
+export GIT_USERNAME=SirKubiac
+export GIT_PASSWORD=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
 
 # Zielverzeichnis und Dateien
 DOH_TARGET="/var/git/Block-Lists/doh_adguard.txt"
-ADS_TARGET="/var/git/Block-Lists/ads_adguard.txt"
-TRACKER_TARGET="/var/git/Block-Lists/tracker_adguard.txt"
+ADS_TRACKER_TARGET="/var/git/Block-Lists/ads_tracker_adguard.txt"
 AMP_TARGET="/var/git/Block-Lists/amp_adguard.txt"
 NATIVE_TARGET="/var/git/Block-Lists/native_tracker_adguard.txt"
-WHITELIST_FILE="/var/filter_lists/whitelist_doh_adguard.txt"
-MICROSOFT_WHITELIST="/var/filter_lists/whitelist_microsoft_adguard.txt"
+WHITELIST_DOH="/var/filter_lists/whitelist_doh_adguard.txt"
+WHITELIST="/var/filter_lists/whitelist_adguard.txt"
 
 # URLs der Listen
 DOH_URL="https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/doh.txt"
-ADS_URLS=(
+ADS_TRACKER_URLS=(
     "https://blocklist.sefinek.net/generated/v1/adguard/ads/0Zinc/easylist.fork.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/ads/adaway/hosts.fork.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/ads/blocklistproject/hosts.fork.txt"
@@ -33,69 +32,57 @@ ADS_URLS=(
     "https://blocklist.sefinek.net/generated/v1/adguard/ads/sefinek.hosts.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/ads/ShadowWhisperer/Ads.fork.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/ads/yoyo/ads-trackers-etc.fork.txt"
-)
-TRACKER_URLS=(
     "https://blocklist.sefinek.net/generated/v1/adguard/tracking-and-telemetry/0Zinc/easyprivacy.fork.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/tracking-and-telemetry/frogeye/firstparty-trackers-hosts.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/tracking-and-telemetry/MajkiIT/adguard-mobile-host.fork.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/tracking-and-telemetry/neodevpro/host.fork.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/tracking-and-telemetry/quidsup/trackers-hosts.fork.txt"
     "https://blocklist.sefinek.net/generated/v1/adguard/tracking-and-telemetry/sefinek.hosts.txt"
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/light.txt"
 )
 AMP_URL="https://blocklist.sefinek.net/generated/v1/adguard/amp/developerdan/amp-hosts-extended.fork.txt"
 NATIVE_URLS=(
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.tiktok.extended.txt"
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.tiktok.txt"
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.samsung.txt"
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.huawei.txt"
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.apple.txt"
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/native.amazon.txt"
 )
 
-# DOH-Blockliste herunterladen und speichern, dabei unerwünschte Zeilen entfernen
+# DOH-Blockliste herunterladen und speichern, dabei unerw nschte Zeilen entfernen
 curl -s "$DOH_URL" | grep -vE '^\!|^\[' > "$DOH_TARGET"
 
 # Whitelist aus der DOH-Liste entfernen
-grep -Fvxf "$WHITELIST_FILE" "$DOH_TARGET" > "$DOH_TARGET.tmp" && mv "$DOH_TARGET.tmp" "$DOH_TARGET"
+grep -Fvxf "$WHITELIST_DOH" "$DOH_TARGET" > "$DOH_TARGET.tmp" && mv "$DOH_TARGET.tmp" "$DOH_TARGET"
 
-# Temporäre Dateien für die Listen
-TEMP_FILE_ADS="$(mktemp)"
-TEMP_FILE_TRACKERS="$(mktemp)"
+# Temporaere Dateien fuer die Listen
+TEMP_FILE_ADS_TRACKER="$(mktemp)"
 TEMP_FILE_AMP="$(mktemp)"
 TEMP_FILE_NATIVE="$(mktemp)"
 
-# Werbeblocker-Listen herunterladen und zusammenfügen
-for URL in "${ADS_URLS[@]}"; do
-    curl -s "$URL" >> "$TEMP_FILE_ADS"
-done
-
-# Tracker-Listen herunterladen und zusammenfügen
-for URL in "${TRACKER_URLS[@]}"; do
-    curl -s "$URL" >> "$TEMP_FILE_TRACKERS"
+# Werbeblocker- und Tracker-Listen herunterladen und zusammenfuegen
+for URL in "${ADS_TRACKER_URLS[@]}"; do
+    curl -s "$URL" >> "$TEMP_FILE_ADS_TRACKER"
 done
 
 # AMP-Liste herunterladen
 curl -s "$AMP_URL" >> "$TEMP_FILE_AMP"
 
-# Nativen Tracker-Listen herunterladen und zusammenfügen
+# Nativen Tracker-Listen herunterladen und zusammenfuegen
 for URL in "${NATIVE_URLS[@]}"; do
     curl -s "$URL" >> "$TEMP_FILE_NATIVE"
 done
 
-# Unerwünschte Zeilen aus den Werbeblocker-Listen entfernen, doppelte Einträge bereinigen und Microsoft-Whitelist berücksichtigen
-grep -vE '^!|^\@\@\|\|' "$TEMP_FILE_ADS" | sort -u | grep -v -f "$MICROSOFT_WHITELIST" > "$ADS_TARGET"
-
-# Unerwünschte Zeilen aus den Tracker-Listen entfernen, doppelte Einträge bereinigen und Microsoft-Whitelist berücksichtigen
-grep -vE '^!|^\@\@\|\|' "$TEMP_FILE_TRACKERS" | sort -u | grep -v -f "$MICROSOFT_WHITELIST" > "$TRACKER_TARGET"
-
-# Unerwünschte Zeilen aus der AMP-Liste entfernen und doppelte Einträge bereinigen
+# Unerwuenschte Zeilen aus den Werbeblocker- und Tracker-Listen entfernen, doppelte Eintraege bereinigen und Microsoft-Whitelist beruecksichtigen
+grep -vE '^!|^@@\|\|' "$TEMP_FILE_ADS_TRACKER" | sort -u | grep -v -f "$WHITELIST" > "$ADS_TRACKER_TARGET"
+# Unerwuenschte Zeilen aus der AMP-Liste entfernen und doppelte Eintraege bereinigen
 grep -vE '^!' "$TEMP_FILE_AMP" | sort -u > "$AMP_TARGET"
 
-# Unerwünschte Zeilen aus den nativen Tracker-Listen entfernen, doppelte Einträge bereinigen und sortieren
+# Unerwuenschte Zeilen aus den nativen Tracker-Listen entfernen, doppelte Eintraege bereinigen und sortieren
 grep -vE '^!|^\[' "$TEMP_FILE_NATIVE" | sort -u > "$NATIVE_TARGET"
 
-# Temporäre Dateien entfernen
-rm -f "$TEMP_FILE_ADS" "$TEMP_FILE_TRACKERS" "$TEMP_FILE_AMP" "$TEMP_FILE_NATIVE"
+# Temporaere Dateien entfernen
+rm -f "$$TEMP_FILE_ADS_TRACKER" "$TEMP_FILE_AMP" "$TEMP_FILE_NATIVE"
 
 # Update the Git repository
 cd /var/git/Block-Lists
